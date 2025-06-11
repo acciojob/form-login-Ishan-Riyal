4e857d5116bfc9a3d8cf746d980bb811280f5603
Ishan Riyal <ishanriyal07@gmail.com>
2025-06-11 15:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
-    //Write your code here
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const btnSubmit = document.querySelector(".btn-submit");
 
+btnSubmit.addEventListener("click", getFormvalue);
+
+function getFormValue() {
+  alert(
+    `${firstName.value} ${lastName.value}`
+  );
 }
